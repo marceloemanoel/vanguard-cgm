15d0289571ec415a1b8e527301ba26fea3fb237b
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2023-12-19 01:50:15 -0300

    Add 404.html so github can mimic a SPA behaviour

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +20,15 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: [
+        path.resolve(__dirname, "./index.html"),
+        path.resolve(__dirname, "./404.html"),
+      ],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
