4e9c1b4734b176e56ea477069925e67746b7dfc8
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2023-12-19 00:22:53 -0300

    Fix deployment to gitub pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vangard/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
