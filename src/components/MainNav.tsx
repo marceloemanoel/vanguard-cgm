9d81105c5510f918d16d2b7af015d3c3974668e9
Marcelo Emanoel Bezerra Diniz <marceloemanoel@gmail.com>
2023-12-21 19:14:35 -0300

    Install husky and lint-staged

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -7,6 +7,7 @@ import {
   PizzaIcon,
   SettingsIcon,
 } from "lucide-react";
+import { FormattedMessage } from "react-intl";
 import { NavLink } from "react-router-dom";
 
 export function MainNav({
@@ -16,7 +17,7 @@ export function MainNav({
   return (
     <nav
       className={cn(
-        "flex items-center justify-around gap-6 space-x-4 lg:space-x-6 h-full",
+        "flex h-full items-center justify-around gap-6 space-x-4 lg:space-x-6",
         className,
       )}
       {...props}
@@ -26,78 +27,102 @@ export function MainNav({
         end
         className={({ isActive }) =>
           cn(
-            "flex flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary h-full",
+            "flex h-full flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary",
             isActive ? "text-primary" : "",
           )
         }
       >
         <span className="flex items-center gap-1">
-          <HomeIcon /> Home
+          <HomeIcon />{" "}
+          <FormattedMessage
+            defaultMessage="Home"
+            description="Home link at the top level navigation"
+          />
         </span>
       </NavLink>
       <NavLink
         to={`${import.meta.env.BASE_URL}/careportal`}
         className={({ isActive }) =>
           cn(
-            "flex flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary h-full",
+            "flex h-full flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary",
             isActive ? "text-primary" : "",
           )
         }
       >
         <span className="flex items-center gap-1">
-          <CrossIcon /> Care Portal
+          <CrossIcon />{" "}
+          <FormattedMessage
+            defaultMessage="Care Portal"
+            description="Care Portal link at the top level navigation"
+          />
         </span>
       </NavLink>
       <NavLink
         to={`${import.meta.env.BASE_URL}/bolus-wizard`}
         className={({ isActive }) =>
           cn(
-            "flex flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary h-full",
+            "flex h-full flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary",
             isActive ? "text-primary" : "",
           )
         }
       >
         <span className="flex items-center gap-1">
-          <CalculatorIcon /> Bolus Wizard
+          <CalculatorIcon />{" "}
+          <FormattedMessage
+            defaultMessage="Bolus Wizard"
+            description="Bolus Wizard link at the top level navigation"
+          />
         </span>
       </NavLink>
       <NavLink
         to={`${import.meta.env.BASE_URL}/food`}
         className={({ isActive }) =>
           cn(
-            "flex flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary h-full",
+            "flex h-full flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary",
             isActive ? "text-primary" : "",
           )
         }
       >
         <span className="flex items-center gap-1">
-          <PizzaIcon /> Food
+          <PizzaIcon />{" "}
+          <FormattedMessage
+            defaultMessage="Food"
+            description="Food link at the top level navigation"
+          />
         </span>
       </NavLink>
       <NavLink
         to={`${import.meta.env.BASE_URL}/reports`}
         className={({ isActive }) =>
           cn(
-            "flex flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary h-full",
+            "flex h-full flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary",
             isActive ? "text-primary" : "",
           )
         }
       >
         <span className="flex items-center gap-1">
-          <FileSpreadsheetIcon /> Reports
+          <FileSpreadsheetIcon />{" "}
+          <FormattedMessage
+            defaultMessage="Reports"
+            description="Reports link at the top level navigation"
+          />
         </span>
       </NavLink>
       <NavLink
         to={`${import.meta.env.BASE_URL}/settings`}
         className={({ isActive }) =>
           cn(
-            "flex flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary h-full",
+            "flex h-full flex-col justify-center px-4 text-sm font-medium transition-colors hover:text-primary",
             isActive ? "text-primary" : "",
           )
         }
       >
         <span className="flex items-center gap-1">
-          <SettingsIcon /> Settings
+          <SettingsIcon />{" "}
+          <FormattedMessage
+            defaultMessage="Settings"
+            description="Settings link at the top level navigation"
+          />
         </span>
       </NavLink>
     </nav>
